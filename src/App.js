beec5eba35de918b7c17979c7952b9142078ee50
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-03-11 18:58:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,112 @@
+import React from "react";
+import "./App.css";
+import ReactMapGl, { Marker, Popup } from "react-map-gl";
+
+import CDCdata from "./SingleState";
+
+const token =
+  "pk.eyJ1Ijoia2ttYXJ0aW5lejk1IiwiYSI6ImNrN25wOHh5MTAwd3czc25pOXplYnpkazMifQ.8lB9XIkOiTxH3PUadXUVdg";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: CDCdata,
+      // loaded: false,
+      newYork: [
+        "New York",
+        "2020-03-10T17:13:27",
+        173,
+        0,
+        0,
+        42.1657,
+        -74.9481
+      ],
+      viewport: {
+        latitude: 39.381266,
+        longitude: -97.922211,
+        width: "100vw",
+        height: "100vw",
+        zoom: 3
+      },
+      selectedState: null
+    };
+  }
+
+  render() {
+    let { data } = this.state;
+
+    // data.splice(52).sort();
+
+    console.log("DATA", data);
+    let date = new Date(this.state.newYork[1]);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <nav></nav>
+          <h1>Coro...NAH..</h1>
+          <img
+            src="https://thumbs.gfycat.com/PinkBraveCobra-size_restricted.gif"
+            alt=""
+          ></img>
+          <h3>
+            {this.state.newYork[0]} as of {date.toDateString()}
+          </h3>
+          <ul>
+            <li>Confirmed: {this.state.newYork[2]}</li>
+            <li>Deaths: {this.state.newYork[3]}</li>
+            <li>Recovered: {this.state.newYork[4]}</li>
+          </ul>
+
+          <div className="App-header" id="map">
+            <ReactMapGl
+              {...this.state.viewport}
+              mapboxApiAccessToken={token}
+              mapStyle="mapbox://styles/kkmartinez95/ck7npqtrh0cef1ir1ir9opgk8"
+              onViewportChange={viewport => {
+                this.setState({ viewport });
+              }}
+            >
+              {data.map((state, ind) => {
+                return (
+                  <Marker
+                    key={ind}
+                    latitude={state.latitude}
+                    longitude={state.longitude}
+                  >
+                    <button
+                      className="sick"
+                      onClick={e => {
+                        e.preventDefault();
+                        this.setState({ selectedState: state });
+                      }}
+                    >
+                      <img src="./sick.png"></img>
+                    </button>
+                  </Marker>
+                );
+              })}
+              {this.state.selectedState ? (
+                <Popup
+                  latitude={this.state.selectedState.latitude}
+                  longitude={this.state.selectedState.longitude}
+                  onClose={() => {
+                    this.setState({ selectedState: null });
+                  }}
+                >
+                  <div>
+                    <h2>{this.state.selectedState.state}</h2>
+                    <p>Cases: {this.state.selectedState.cases} </p>
+                  </div>
+                </Popup>
+              ) : null}
+            </ReactMapGl>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/SingleState.js
@@ -0,0 +1,335 @@
+const CDCdata = [
+  {
+    state: "Wyoming",
+    cases: "None",
+    latitude: 43.07597,
+    longitude: -107.290283
+  },
+  {
+    state: "West Virginia",
+    cases: "None",
+    latitude: 39.0,
+    longitude: -80.5
+  },
+
+  {
+    state: "Washington",
+    cases: 267,
+    latitude: 47.751076,
+    longitude: -120.740135
+  },
+
+  {
+    state: "Texas",
+    cases: 15,
+    latitude: 31.0,
+    longitude: -100.0
+  },
+
+  {
+    state: "Pennsylvania",
+    cases: 12,
+    latitude: 41.203323,
+    longitude: -77.194527
+  },
+
+  {
+    state: "Virginia",
+    cases: 8,
+    latitude: 37.926868,
+    longitude: -78.024902
+  },
+
+  {
+    state: "North Dakota",
+    cases: "None",
+    latitude: 47.650589,
+    longitude: -100.437012
+  },
+
+  {
+    state: "Wisconsin",
+    cases: "1 to 5",
+    latitude: 44.5,
+    longitude: -89.5
+  },
+
+  {
+    state: "New York",
+    cases: 173,
+    latitude: 43.0,
+    longitude: -75.0
+  },
+  {
+    state: "Oregon",
+    cases: 15,
+    latitude: 44.0,
+    longitude: -120.5
+  },
+
+  {
+    state: "New Mexico",
+    cases: "None",
+    latitude: 34.307144,
+    longitude: -106.018066
+  },
+  {
+    state: "New Jersey",
+    cases: 15,
+    latitude: 39.833851,
+    longitude: -74.871826
+  },
+  {
+    state: "South Carolina",
+    cases: 9,
+    latitude: 33.836082,
+    longitude: -81.163727
+  },
+
+  {
+    state: "Tennessee",
+    cases: 7,
+    latitude: 35.860119,
+    longitude: -86.660156
+  },
+
+  {
+    state: "Montana",
+    cases: "None",
+    latitude: 46.96526,
+    longitude: -109.533691
+  },
+  {
+    state: "Mississippi",
+    cases: "None",
+    latitude: 33.0,
+    longitude: -90.0
+  },
+
+  {
+    state: "Ohio",
+    cases: "1 to 5",
+    latitude: 40.367474,
+    longitude: -82.996216
+  },
+
+  {
+    state: "New Hampshire",
+    cases: "1 to 5",
+    latitude: 44.0,
+    longitude: -71.5
+  },
+
+  {
+    state: "Nevada",
+    cases: "1 to 5",
+    latitude: 39.876019,
+    longitude: -117.224121
+  },
+
+  {
+    state: "Nebraska",
+    cases: "1 to 5",
+    latitude: 41.5,
+    longitude: -100.0
+  },
+
+  {
+    state: "Massachusetts",
+    cases: 91,
+    latitude: 42.407211,
+    longitude: -71.382439
+  },
+
+  {
+    state: "Maine",
+    cases: "None",
+    latitude: 45.367584,
+    longitude: -68.972168
+  },
+
+  {
+    state: "Illinois",
+    cases: 19,
+    latitude: 40.0,
+    longitude: -89.0
+  },
+
+  {
+    state: "Maryland",
+    cases: 9,
+    latitude: 39.045753,
+    longitude: -76.641273
+  },
+
+  {
+    state: "North Carolina",
+    cases: 7,
+    latitude: 35.782169,
+    longitude: -80.793457
+  },
+
+  {
+    state: "Idaho",
+    cases: "None",
+    latitude: 44.068203,
+    longitude: -114.742043
+  },
+
+  {
+    state: "Delaware",
+    cases: "None",
+    latitude: 39.0,
+    longitude: -75.5
+  },
+  {
+    state: "Vermont",
+    cases: "1 to 5",
+    latitude: 44.0,
+    longitude: -72.699997
+  },
+  {
+    state: "Utah",
+    cases: "1 to 5",
+    latitude: 39.41922,
+    longitude: -111.950684
+  },
+
+  {
+    state: "South Dakota",
+    cases: "1 to 5",
+    latitude: 44.5,
+    longitude: -100.0
+  },
+  {
+    state: "Rhode Island",
+    cases: "1 to 5",
+    latitude: 41.700001,
+    longitude: -71.5
+  },
+
+  {
+    state: "Oklahoma",
+    cases: "1 to 5",
+    latitude: 36.084621,
+    longitude: -96.921387
+  },
+
+  {
+    state: "Missouri",
+    cases: "1 to 5",
+    latitude: 38.573936,
+    longitude: -92.60376
+  },
+  {
+    state: "Minnesota",
+    cases: "1 to 5",
+    latitude: 46.39241,
+    longitude: -94.63623
+  },
+  {
+    state: "Michigan",
+    cases: "1 to 5",
+    latitude: 44.182205,
+    longitude: -84.506836
+  },
+  {
+    state: "Louisiana",
+    cases: "1 to 5",
+    latitude: 30.39183,
+    longitude: -92.329102
+  },
+
+  {
+    state: "Kentucky",
+    cases: "1 to 5",
+    latitude: 37.839333,
+    longitude: -84.27002
+  },
+
+  {
+    state: "Kansas",
+    cases: "1 to 5",
+    latitude: 38.5,
+    longitude: -98.0
+  },
+  {
+    state: "Indiana",
+    cases: "1 to 5",
+    latitude: 40.273502,
+    longitude: -86.126976
+  },
+  {
+    state: "Hawaii",
+    cases: "1 to 5",
+    latitude: 19.741755,
+    longitude: -155.844437
+  },
+  {
+    state: "Connecticut",
+    cases: "1 to 5",
+    latitude: 41.599998,
+    longitude: -72.699997
+  },
+  {
+    state: "California",
+    cases: 152,
+    latitude: 36.778259,
+    longitude: -119.417931
+  },
+  {
+    state: "Georgia",
+    cases: 23,
+    latitude: 33.247875,
+    longitude: -83.441162
+  },
+  {
+    state: "Florida",
+    cases: 19,
+    latitude: 27.994402,
+    longitude: -81.760254
+  },
+
+  {
+    state: "Colorado",
+    cases: 17,
+    latitude: 39.113014,
+    longitude: -105.358887
+  },
+
+  {
+    state: "Iowa",
+    cases: 13,
+    latitude: 42.032974,
+    longitude: -93.581543
+  },
+
+  {
+    state: "Arkansas",
+    cases: "None",
+    latitude: 34.799999,
+    longitude: -92.199997
+  },
+
+  {
+    state: "Arizona",
+    cases: 8,
+    latitude: 34.048927,
+    longitude: -111.093735
+  },
+  {
+    state: "Alaska",
+    cases: "None",
+    latitude: 66.160507,
+    longitude: -153.369141
+  },
+
+  {
+    state: "Alabama",
+    cases: "None",
+    latitude: 32.31823,
+    longitude: -86.902298
+  }
+];
+
+export default CDCdata;
