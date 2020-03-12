2f43c669f2691e9b793b9f6b284df954824982b9
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-03-11 20:01:44 -0400

    Updates

---
+++ precache-manifest.574b4957ab0c29bcfc0b426ff580d124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c353c23b92016aa71b5134dd6e5707",
+    "url": "/coro-nah/index.html"
+  },
+  {
+    "revision": "49c6ca7f639cece885e9",
+    "url": "/coro-nah/static/css/main.565319d6.chunk.css"
+  },
+  {
+    "revision": "5ded2513b8fc4316b430",
+    "url": "/coro-nah/static/js/2.41483f76.chunk.js"
+  },
+  {
+    "revision": "7bfcea31855bd57860628e5726359b29",
+    "url": "/coro-nah/static/js/2.41483f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c6ca7f639cece885e9",
+    "url": "/coro-nah/static/js/main.d8aa1f96.chunk.js"
+  },
+  {
+    "revision": "b2fe1bfca8c2b77fa73a",
+    "url": "/coro-nah/static/js/runtime-main.190f9974.js"
+  }
+]);
